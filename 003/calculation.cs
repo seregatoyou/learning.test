7c16b0d5f84e3a3cfa8f4c710db002712d1dd41c
IT <spanyukhin@list.ru>
2023-10-08 06:48:13 +0300

    003 minor modification

--- 003/calculation.cs
+++ 003/calculation.cs
@@ -1,3 +1,5 @@
-int numberA = 3;
-int numberB = 8;
+int numberA = new Random().Next(1,10); // 1 2 3 4 ...
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,20);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
