b62f383bb47aa8b03d0bd797b6cf90f8cbe50a8f
IT <spanyukhin@list.ru>
2023-10-08 07:05:31 +0300

    Masha

--- 005/Program.cs
+++ 005/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha"){
+    Console. WriteLine("это же МАША!");
+}
+else {
+    Console.WriteLine("ааа, это не Маша...");
+    Console.Write("Привет.");
+};
