50f31ce7874e64b1a4549569b7e2bf8240a4f9ca
IT <spanyukhin@list.ru>
2023-10-08 06:23:52 +0300

    add calculation.cs

---
+++ 003/calculation.cs
@@ -0,0 +1,6 @@
+Console.WriteLine("Ввод чисел: ");
+Console.Write("Введите первое число ");
+int first_number = Console.ReadLine();
+Console.WriteLine("Введите первое число ");
+int second_number = Console.ReadLine();
+Console.WriteLine("Сумма чисел: " first_number + second_number);
